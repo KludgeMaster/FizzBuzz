b64048844dc475260ff34c80f52a4c629403259d
KludgeMaster <yiryans@gmai.com>
2015-07-21 19:09:26 +0000

    FizzBuzz Refactored

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,17 +1,22 @@
-(1..100).each do |x|
-	my_string = ""
-	if x % 3 == 0
-		my_string << "fizz"
-	end
+def fizzbuzz(a,b,str1="fizz", str2="buzz")
+  puts ('Welcome to FizzBuzz!!!')
+  (a..b).each do |x|
+    my_string = ""
+    if x % 3 == 0
+      my_string << str1
+    end
 
-	if x % 5 == 0
-		my_string << "buzz"
-	end
+    if x % 5 == 0
+      my_string << str2
+    end
 
-	if my_string == ""
-		my_string = "#{x}"
-	end
+    if my_string == ""
+      my_string = "#{x}"
+    end
 
-	puts my_string
-
+    puts my_string
+  end
 end
+
+fizzbuzz(3,20)
+fizzbuzz(5,17,"three?","five?")
